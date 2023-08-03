542d1ffd8ab18aaeea1913e29a432969450bf580
HenryCode4 <henrycnnamani4@gmail.com>
2023-08-02 22:14:30 -0700

    added a lot with comment section

---
+++ components/Avatar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Avatar = () => {
+  return (
+    <div className="w-12 h-12 rounded-full overflow-hidden">
+    <img
+      src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60"
+      alt=""
+    />
+  </div>
+  )
+}
+
+export default Avatar
