9841f48f089ed40c6c33bc0734ec9600d9248050
HenryCode4 <henrycnnamani4@gmail.com>
2023-08-04 09:34:56 -0700

    latest commit

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,14 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
 
-const Avatar = () => {
+const Avatar = ({size}) => {
+  let width = 'w-12';
+  let height = 'h-12';
+
+  if(size === 'lg'){
+    width= "w-32"
+  }
+
+  if(size === 'lg'){
+    height= "h-32"
+  }
   return (
-    <div className="overflow-hidden w-12 h-12 rounded-full">
+    <div className={`overflow-hidden ${width} ${height} rounded-full relative`}>
     <Image
       src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60"
       alt=""
-      width={60}
-        height={60}
+      fill
       className="object-cover rounded-full"
     />
   </div>
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -8,19 +8,34 @@ const ProfilePage = () => {
   return (
     <Layout>
         <Card noPadding={true}>
-            <div className='h-48 overflow-hidden flex justify-center items-center'>
-                <Image src="https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YWZyaWNhfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60" 
+            <div className="relative overflow-hidden rounded-md">
+                <div className='h-48 overflow-hidden flex justify-center items-center relative'>
+            <Image src="https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YWZyaWNhfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60" 
             alt=''
-            height={192}
-            width={700}
+            fill
             className='object-cover'
             />
             </div>
-            
-            <div className='p-4'>
-                <Avatar />
-                Chima Nnamani
+            <div className='absolute top-36 left-4'>
+                <Avatar size={"lg"} />
             </div>
+
+            <div className='p-4 pb-24'>
+                <div className='ml-40'>
+                    <h1 className=' text-3xl font-bold'>Chima Nnamani </h1>
+                <div className='text-gray-400 leading-4'>
+                    Stockhole, Sweden
+                </div>
+                </div>
+                
+            </div>
+            </div>
+            
+
+            
+            
+            
+            
             
         </Card>
     </Layout>
